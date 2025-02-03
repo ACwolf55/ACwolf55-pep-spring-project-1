71d631428a576338d55e0d7d9986844c4cd918f9
AC <Alexcwolf5@gmail.com>
2025-02-03 02:04:55 +0000

    account service and start message service

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,7 +1,58 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
 public class AccountService {
 
+    AccountRepository accountRepository;
 
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account register(Account account){
+        String username= account.getUsername();
+        boolean userExists = accountRepository.existByUsername(username);
+        if(userExists){
+            return null;
+        }
+        if(account.getUsername()==null || account.getUsername().isEmpty()){
+            throw new IllegalArgumentException("Username cannot be blank");
+        }
+        if(account.getPassword()==null || account.getPassword().length() < 4){
+            throw new IllegalArgumentException("Password must be at least 4 characters long");
+        }
+        
+        return accountRepository.save(account);
+
+    }
+
+    public Optional<Account> login(Account account){
+        String username = account.getUsername();
+        String password = account.getPassword();
+
+        Account authAccount = accountRepository.findByUsername(username);
+
+
+        if(authAccount != null && authAccount.getPassword().equals(password)){
+            return Optional.of(authAccount);
+        }else{
+            return Optional.empty();
+        }
+
+    }
     
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,8 +1,42 @@
 package com.example.service;
 
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+@Transactional
 public class MessageService {
 
+    private final MessageRepository messageRepository;
+    private final AccountRepository accountRepository;
 
+    @Autowired 
+    public MessageService(MessageRepository messageRepository,AccountRepository accountRepository){
+        this.messageRepository = messageRepository;
+        this.accountRepository = accountRepository;
+    }
+
+
+    public Message postMessage(Message message){
+        int account_id = message.getPostedBy();
+        boolean userExists = accountRepository.existById(account_id);
+        if (message.getMessageText()==null || message.getMessageText().trim().isEmpty() || message.getMessageText.length() >225 || !userExists){
+            throw new IllegalArgumentException("INvalid message or user");
+        }
+        return messageRepository.save(message);
+
+    }
 
     
 }
